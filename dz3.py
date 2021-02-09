dd18f99983b54b8ac3002cf9b79633cf832ed0c7
ato13 <Kjk80aff>
2021-02-09 10:49:38 +0200

    добавил дз

---
+++ dz3.py
@@ -0,0 +1,19 @@
+value1 = int(input("Введите число:"))
+new_value1 = value1 / 2 if value1 < 100 else (-value1)
+print(new_value1)
+##############################
+value2 = int(input("Введите число:"))
+new_value2 = 1 if value2 < 100 else 0
+print(new_value2)
+##############################
+value3 = int(input("Введите число:"))
+new_value3 = True if value3 < 100 else False
+print(new_value3)
+###############################
+my_str1 = str(input("Введите строку:"))
+print(my_str1.upper())
+##############################
+my_str1 = str(input("Введите строку:"))
+print(my_str1.lower())
+###############################
+
